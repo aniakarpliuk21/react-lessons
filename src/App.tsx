25e1ff3eb93be2d1faf37faa2d9dac7144234ef3
aniakarpliuk21 <aniakarpluk21@gmail.com>
2024-05-13 22:16:55 +0300

    HomeWork2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React, {FC} from 'react';
-import './App.css';
+import UsersComponent from "./components/UsersComponent";
+import PostsComponent from "./components/PostsComponent";
+import {getAllPostOfUser} from "./services/jpg.api.services";
+import {useState} from "react";
+import {IPostModel} from "./models/IPostModel";
 
- const App: FC = () => {
+
+const App = () => {
+    const [posts,setPosts] = useState<IPostModel[]>([])
+    const lift = (postId:number) => {
+        getAllPostOfUser(postId).then(({data})=>{
+            setPosts(data)})
+    }
   return (
-    <>
-    </>
-  );
+    <div className={'all'}>
+        <div className={'users'}>
+            <UsersComponent  lift={lift}/>
+        </div>
+        <div className={'posts'}>
+            <PostsComponent posts={posts}/>
+        </div>
+    </div>
+  )
 }
 
 export default App;
